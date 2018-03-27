14488e7a2271063596f07069e86da33fd5b643da
Victor Hugo Soares Cardoso <victorhugo_nf@hotmail.com>
2018-03-27 01:29:05 -0300

    created InitializeDatabaseService and setup pom

--- TemplateJavaEE/ejb/src/main/java/io/github/victorhugonf/javaee/ejb/service/InitializeDatabaseService.java
+++ TemplateJavaEE/ejb/src/main/java/io/github/victorhugonf/javaee/ejb/service/InitializeDatabaseService.java
@@ -12,7 +12,7 @@ import io.github.victorhugonf.javaee.ejb.entity.Industry;
 @Singleton
 @LocalBean
 @Startup
-public class InitializeDbHyperSonicSqlService {
+public class InitializeDatabaseService {
 	   
 	@EJB
 	private IndustryService industryService;
@@ -21,31 +21,38 @@ public class InitializeDbHyperSonicSqlService {
 	private AircraftService aircraftService;
 	
 	@PostConstruct
-	private void initialize(){
+	private void initialize() throws Exception{
 		Industry boeing = new Industry();
 		boeing.setName("Boeing");
+		industryService.persist(boeing);
 		
 		Industry embraer = new Industry();
 		embraer.setName("Embraer");
+		industryService.persist(embraer);
 		
 		Industry airbus = new Industry();
 		airbus.setName("Airbus");
+		industryService.persist(airbus);
 		
 		Aircraft b773 = new Aircraft();
 		b773.setModel("777-300");
 		b773.setIndustry(boeing);
+		aircraftService.persist(b773);
 		
 		Aircraft b789 = new Aircraft();
 		b789.setModel("787-9");
 		b789.setIndustry(boeing);
+		aircraftService.persist(b789);
 		
 		Aircraft a388 = new Aircraft();
 		a388.setModel("A380-800");
 		a388.setIndustry(airbus);
+		aircraftService.persist(a388);
 		
 		Aircraft e195 = new Aircraft();
 		e195.setModel("E-jet 195");
 		e195.setIndustry(embraer);
+		aircraftService.persist(e195);
 	}
     
 }
---
+++ TemplateJavaEE/rest-api/src/main/java/io/github/victorhugonf/javaee/EndPointAircraft.java
@@ -0,0 +1,27 @@
+package io.github.victorhugonf.javaee;
+
+import javax.inject.Inject;
+import javax.ws.rs.Path;
+
+import io.github.victorhugonf.javaee.ejb.entity.Aircraft;
+import io.github.victorhugonf.javaee.ejb.service.AircraftService;
+
+@Path("aircraft")
+public class EndPointAircraft extends GenericEndPoint<Aircraft, AircraftService> {
+
+	@Inject
+	private AircraftService aircraftService;
+
+	@Override
+	protected AircraftService service() {
+		return aircraftService;
+	}
+
+	@Override
+	protected Class<Aircraft> getClazz() {
+		return Aircraft.class;
+	}
+
+
+
+}
